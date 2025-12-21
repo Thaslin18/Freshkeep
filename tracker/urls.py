b46fbf52ee341ed1e4bd3076af718f5658f96e02
Thaslin18 <thaslin18parveen@gmail.com>
2025-12-21 20:20:52 +0530

    Fix formatting of dashboard URL path

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('',views.dashboard, name='dashboard'),
     path('finish/<int:pk>/', views.finish_item, name='finish_item'),
 ]
