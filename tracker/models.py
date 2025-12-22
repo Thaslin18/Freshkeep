8a01c036c6ea05da4364c961f5d94ef3bd2992c4
Thaslin18 <thaslin18parveen@gmail.com>
2025-12-22 22:49:20 +0530

    Update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -3,12 +3,6 @@ from django.contrib.auth.models import User
 
 class PantryItem(models.Model):
     # Making the user field optional so you can save without logging in
-    user = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        null=True, 
-        blank=True
-    )
     name = models.CharField(max_length=100)
     expiry_date = models.DateField()
 
